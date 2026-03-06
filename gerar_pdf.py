01b2fa527ecda651e044ffb7fe8b1592278de955
Fonseca2025 <avilofonseca@gmail.com>
2026-03-06 09:07:55 -0300

    Create gerar_pdf.py

---
+++ gerar_pdf.py
@@ -0,0 +1,18 @@
+import json
+from fpdf import FPDF
+
+with open("eventos.json") as f:
+    eventos = json.load(f)
+
+pdf = FPDF()
+pdf.add_page()
+
+pdf.set_font("Arial", size=14)
+
+pdf.cell(200,10,"Agenda Paroquial",ln=True)
+
+for e in eventos:
+    linha = f'{e["data"]} - {e["hora"]} - {e["evento"]} - {e["local"]}'
+    pdf.cell(200,10,linha,ln=True)
+
+pdf.output("agenda_paroquial.pdf")
