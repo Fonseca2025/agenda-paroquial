8fbd98e8d3edbc5f8645aeb1a3d2c6eac8487648
Fonseca2025 <avilofonseca@gmail.com>
2026-03-06 09:53:22 -0300

    Update gerar_pdf.py

--- gerar_pdf.py
+++ gerar_pdf.py
@@ -12,7 +12,7 @@ pdf.set_font("Arial", size=14)
 pdf.cell(200,10,"Agenda Paroquial",ln=True)
 
 for e in eventos:
-    linha = f'{e["data"]} - {e["hora"]} - {e["evento"]} - {e["local"]}'
+    linha = f'{e["data"]} - {e.get("hora", "--:--")} - {e["evento"]} - {e["local"]}'
     pdf.cell(200,10,linha,ln=True)
 
 pdf.output("agenda_paroquial.pdf")
